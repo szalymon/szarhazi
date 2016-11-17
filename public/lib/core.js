96795ab645d5276b9af2945264fa26e747092914
Simon Urbanovics <urbsimon@gmail.com>
2016-11-17 17:12:31 +0100

    Canvas with paperjs

--- public/lib/core.js
+++ public/lib/core.js
@@ -1,16 +1,19 @@
-var paper = require('paper');
-
-$(function(){
+$(function () {
     console.log("Client is running...");
 
-    // Create a Paper.js Path to draw a line into it:
-	var path = new Path();
-	// Give the stroke a color
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	// Move to start and draw a line from there
-	path.moveTo(start);
-	// Note the plus operator on Point objects.
-	// PaperScript does that for us, and much more!
-	path.lineTo(start + [ 100, -50 ]);
+    var canvas = document.getElementById('mainCanvas');
+
+    paper.setup(canvas);
+
+    var path = new paper.Path();
+    // Give the stroke a color
+    path.strokeColor = 'black';
+    var start = new paper.Point(100, 100);
+    // Move to start and draw a line from there
+    path.moveTo(start);
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([200, -50]));
+    // Draw the view now:
+    paper.view.draw();
 });
